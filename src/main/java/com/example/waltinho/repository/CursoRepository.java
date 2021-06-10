696e05fb056446ffa48f130cbb8df81c4c482fcb
Walter Ferreira <69780634+FerreiraWalter@users.noreply.github.com>
2021-06-09 22:08:11 -0300

    Add files via upload

---
+++ src/main/java/com/example/waltinho/repository/CursoRepository.java
@@ -0,0 +1,11 @@
+package com.example.waltinho.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.waltinho.model.Curso;
+
+@Repository
+public interface CursoRepository extends  JpaRepository<Curso, Long> {
+
+}
---
+++ src/test/java/com/example/waltinho/WaltinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waltinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaltinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
